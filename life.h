a131a6849e457107be9b84b82430deb67aa51c6f
Akash Shaji <akashshaji321@gmail.com>
2018-01-29 05:31:53 -0500

    added life.h

---
+++ life.h
@@ -0,0 +1,11 @@
+#ifndef LIFE_H_
+#define LIFE_H_
+
+int main(int argc, char **argv);
+int** emulateBoard(int** board, int rows, int columns);
+void printBoard(int** board, int rows, int columns);
+int checkTermination(int** boardA, int** boardB, int rows, int columns);
+	
+	
+	
+#endif
